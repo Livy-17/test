d5c289c0b38196e409bb4ba864c03719ee6de491
Liwei <42135339+Livy-17@users.noreply.github.com>
2022-08-09 00:14:48 +0100

    styles changed

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './Body.css';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const Body = () => {
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const formSchema = Yup.object().shape(
+    {
+      firstname: Yup.string()
+        .required('Firstname is mendatory')
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+      lastname: Yup.string()
+        .required('Lastname is mendatory')
+        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field "),
+      email: Yup.string()
+        .required('Email is mendatory')
+        .email('Invalid email format'),
+      password: Yup.string()
+        .required('Password is mendatory')
+        .min(3, 'Passwords must be at least 3 characters long'),
+      confirmPwd: Yup.string()
+        .required('Password is mendatory')
+        .oneOf([Yup.ref('password')], 'Passwords does not match')
+    }
+  )
+
+  const formOptions = { resolver: yupResolver(formSchema) };
+
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+
+  const { errors } = formState;
 
   const onSubmit = (data) => {
     console.log(data);
@@ -16,30 +42,37 @@ const Body = () => {
         <h2>Sign Up Your Account</h2>
         <form onSubmit={handleSubmit(onSubmit)} className='form--container'>
           <div className='input--container'>
-            <label htmlFor='firstName' className='labels'>First Name:</label>
-            <input className='inputFrame' id='firstName' {...register("firstname", { required: true, pattern: /^[A-Za-z]+$/i })} />
-            {errors.firstname && <p>firstname error</p>}
+            <label htmlFor='FirstName' className='labels'>First Name:</label>
+            <input className={errors.firstname ? 'inputFrame--red' : 'inputFrame'} id='FirstName' type='text' name='firstname' {...register("firstname")} />
+            {errors.firstname && <p className='error--info'>Please enter a valid firstname.</p>}
           </div>
           <div className='input--container'>
-            <label htmlFor='lastName' className='labels'>Last Name:</label>
-            <input className='inputFrame' id='lastName' {...register("lastname", { required: true, pattern: /^[A-Za-z]+$/i })} />
-            {errors.lastname && <p>lastname error</p>}
+            <label htmlFor='LastName' className='labels'>Last Name:</label>
+            <input className={errors.lastname ? 'inputFrame--red' : 'inputFrame'} id='LastName' type='text' name='lastname' {...register("lastname")} />
+            {errors.lastname && <p className='error--info'>Please enter a valid lastname.</p>}
           </div>
           <div className='input--container'>
             <label htmlFor='Email' className='labels'>Email:</label>
-            <input className='inputFrame' id='email' {...register("email", { required: true, pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "invalid email address" } })} />
-            {errors.email && <p>email error</p>}
+            <input className={errors.email ? 'inputFrame--red' : 'inputFrame'} id='Email' type='email' name='email' {...register("email")} />
+            {errors.email && <p className='error--info'>Please enter a valid email address.</p>}
           </div>
           <div className='input--container'>
             <label htmlFor='Password' className='labels'>Password:</label>
-            <input className='inputFrame' id='password' {...register("password", { required: true })} />
-            {errors.password && <p>password error</p>}
+            <input className={errors.password ? 'inputFrame--red' : 'inputFrame'} id='Password' type='password' name='password' {...register("password")} />
+            {errors.password && <p className='error--info'>Please enter a valid password.</p>}
           </div>
+          <div className='input--container'>
+            <label htmlFor='ConfirmPwd' className='labels'>Password:</label>
+            <input className={errors.confirmPwd ? 'inputFrame--red' : 'inputFrame'} id='ConfirmPwd' type='password' name='confirmPwd' {...register("confirmPwd")} />
+            {errors.confirmPwd && <p className='error--info'>Passwords do not match.</p>}
+          </div>
+
           <input className='submit--btn' type="submit" value='Sign Up' />
         </form>
       </div>
     </div>
   )
+
 };
 
 export default Body;
